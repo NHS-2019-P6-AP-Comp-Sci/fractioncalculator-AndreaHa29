c4eeca22d5864f906ffff0d3f5e23eb4cdfb2496
s-haan <s-haan@53S50023241.it.bsd405.org>
2019-11-15 21:55:36 -0800

    Completed checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,43 @@
 
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		Scanner firstInput = new Scanner(System.in);
+		System.out.println("Welcome to the fraction calculator");
+		System.out.println("Enter the values you want to be calculated");
+		String fraction = firstInput.nextLine();
+		System.out.println(produceAnswer(fraction));
 
-    }
+		// TODO: Read the input from the user and call produceAnswer with an equation
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+	}
 
-        return "";
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		// TODO: Implement this function to produce the solution to the input
+		Scanner math = new Scanner(input);
+		String firstOperand = math.next();
+		String operator = math.next();
+		String secondOperand = math.next();
+		return secondOperand;
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+	}
+
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
 
 }
