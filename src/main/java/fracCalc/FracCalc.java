876bac87aaf45cb1eab26b5f9765a01670f5197d
s-haan <s-haan@53S50023241.it.bsd405.org>
2019-11-25 00:00:32 -0800

    checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -13,7 +13,16 @@ public class FracCalc {
 		System.out.println("Welcome to the fraction calculator");
 		System.out.println("Enter the values you want to be calculated");
 		String fraction = firstInput.nextLine();
+		
+		
+		
+		
+		while(!fraction.equals("quit")) {
+		
 		System.out.println(produceAnswer(fraction));
+		System.out.println("Welcome to the fraction calculator");
+		System.out.println("Enter the values you want to be calculated");
+		fraction = firstInput.nextLine();
 
 		// TODO: Read the input from the user and call produceAnswer with an equation
 
@@ -30,17 +39,92 @@ public class FracCalc {
 	// The function should return the result of the fraction after it has been
 	// calculated
 	// e.g. return ==> "1_1/4"
-	public static String produceAnswer(String input) {
+		public static String produceAnswer(String input){
+		
+		
 		// TODO: Implement this function to produce the solution to the input
-		Scanner math = new Scanner(input);
-		String firstOperand = math.next();
-		String operator = math.next();
-		String secondOperand = math.next();
-		return secondOperand;
-
+		
+		
+		
+		String org = input;
+		
+		
+		String firstOper = org.substring(0, org.indexOf(" "));
+	    org = org.substring(org.indexOf(" ")+1);
+	    
+	    String Operator = org.substring(0,org.indexOf(" "));
+	    org = org.substring(org.indexOf(" ")+1);
+	  
+	    String secondOper = org;
+	    
+	    
+	        String secondWhole = Whole(secondOper); 
+	       String secondNum= Num(secondOper);
+	       String secondDem= Dem(secondOper);
+	           
+	       String secondFull= "whole:" + secondWhole +  " numerator:" + secondNum + " denominator:" + secondDem;
+	       // whole:20 numerator:0 denominator:1
+	       return secondFull;
+	       
+	       
+	    }
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
+	
+	
+    public static String Whole(String str) {
+        
+        if (str.indexOf("_") != -1) {
+        return str.substring(0,str.indexOf("_"));
+    
+        }
+        
+        else if (str.indexOf("/") != -1) {
+        return "0";
+       	
+        }
+        
+        else {
+        return str;
+        }
+        
+    }
+    
+    
+    public static String Num(String str) {
+        if (str.indexOf("_") != -1) {
+        return str.substring(str.indexOf("_")+1,str.indexOf("/"));
+    
+        }
+        
+        else if (str.indexOf("/") != -1) {
+        return str.substring(0,str.indexOf("/"));
+       	
+        }
+        
+        else {
+        return "0";
+        }
+          
+    }
+    
+    public static String Dem(String str) {
+        if (str.indexOf("/") != -1) {
+        return str.substring(str.indexOf("/")+1);
+    
+        }
+        
+        
+        else {
+        return "1";
+        }
+          
+    }
+	
+	
+	
+	
 
 }
